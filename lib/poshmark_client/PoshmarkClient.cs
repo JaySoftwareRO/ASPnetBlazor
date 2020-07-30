1f7c3aaa4ff93d65d9e0eb019b71851a73d234a3
Dan Pavel Sinkovicz <45354872+napelvs@users.noreply.github.com>
2020-07-31 00:54:31 +0300

    Add Poshmark client (#8)

---
+++ lib/poshmark_client/PoshmarkClient.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using HtmlAgilityPack;
+
+namespace lib.poshmark_client
+{
+    public class PoshmarkItem
+    {
+        public string Title { get; set; }
+
+        public double Price { get; set; }
+
+        public bool NotForSale { get; set; }
+
+        public string Size { get; set; }
+
+        public string Brand { get; set; }
+
+        public string ProductPageLink { get; set; }
+
+        public string Description { get; set; }
+
+        public List<string> Categories { get; set; }
+
+        public List<string> Color { get; set; }
+    }
+    public class PoshmarkClient
+    {
+        public PoshmarkItem Item = new PoshmarkItem();
+        // Title of a product
+        public string GetTitle(HtmlAgilityPack.HtmlNode node)
+        {
+            var tempTitle = "";
+            try
+            {
+                tempTitle = node.SelectSingleNode("./div[@class='tile  ']/a").GetAttributeValue("title", "");
+            } 
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Error referring to Title: " + e);
+            }
+            return tempTitle;
+        }
+        // Price of a product
+        public double GetPrice(HtmlAgilityPack.HtmlNode node)
+        {
+            var tempPrice = 0.0;
+            try
+            {
+                tempPrice = Convert.ToDouble(node.SelectSingleNode("./div[@class='tile  ']").GetAttributeValue("data-post-price", "").Trim('$'));
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Error referring to Price: " + e);
+            }
+            return tempPrice;
+        }
+        // NotForSale boolean of a product
+        public bool GetNotForSale(HtmlAgilityPack.HtmlNode node)
+        {
+            var tempBool = false;
+            try
+            {   
+                if (node.SelectSingleNode(".//i[@class='icon inventory-tag not-for-sale-tag']").InnerText == "Not for Sale")
+                {
+                    tempBool = true;
+                } 
+                else
+                {
+                    tempBool = false;
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Error referring to NotForSale: " + e);
+            }
+            return tempBool;
+        }
+        // Size of of a product
+        public string GetSize(HtmlAgilityPack.HtmlNode node)
+        {
+            var tempSize = "";
+            try
+            {
+                tempSize = node.SelectSingleNode("./div[@class='tile  ']/div[@class='item-details']/ul[@class='pipe']/li[@class='size']/div").GetAttributeValue("title", "");
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Error referring to Size: " + e);
+                
+                tempSize = node.SelectSingleNode("./div[@class='tile  ']/div[@class='item-details']/ul[@class='pipe']/li/a").GetAttributeValue("title", "");
+                
+            }
+            return tempSize;
+        }
+        // Brand of a product
+        public string GetBrand(HtmlAgilityPack.HtmlNode node)
+        {
+            var tempBrand = "";
+            try
+            {
+                tempBrand = node.SelectSingleNode("./div[@class='tile  ']/div[@class='item-details']/ul[@class='pipe']/li[@class='brand']/a").GetAttributeValue("title", "");
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Error referring to Brand: " + e);
+            }
+            return tempBrand;
+        }
+
+        // Categories of a product
+        public List<String> GetCategories(HtmlAgilityPack.HtmlNodeCollection nodes)
+        {
+            List<string> categories = new List<string>();
+            try
+            {                
+                foreach (var node in nodes)
+                {
+                    var nodes2 = node.SelectNodes("./div[@class='d--fl fw--w']/div[1]/a");
+
+                    foreach (var node2 in nodes2)
+                    {
+                        categories.Add(node2.SelectSingleNode(".").InnerText.Trim());
+                    }
+                }
+            }
+            catch (ArgumentNullException e)
+            {
+                Console.WriteLine("Error referring to Categories: " + e);
+            }
+            return categories;
+        }
+
+        public string GetDescription(HtmlAgilityPack.HtmlNodeCollection nodes)
+        {
+            var tempDescription = "";
+            try
+            {
+                foreach (var node in nodes)
+                {
+                    tempDescription = node.SelectSingleNode("./div[@class='listing__description fw--light']").InnerText.Trim();
+                }
+            } catch (NullReferenceException e)
+            {
+                Console.WriteLine("Error referring to Description: " + e);
+            }
+            return tempDescription;
+        }
+
+        public string GetProductPageLink(HtmlAgilityPack.HtmlNode node)
+        {
+            var tempLink = "";
+            try
+            {
+                tempLink = node.SelectSingleNode("./div[@data-post-price='$" + Item.Price + "']/a[@title='" + Item.Title + "']").GetAttributeValue("href", "");
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Error referring to GetProductPageLink: " + e);
+            }
+            return tempLink;
+        }
+
+        public List<string> GetColor(HtmlAgilityPack.HtmlNodeCollection nodes)
+        {
+            List<string> colors = new List<string>();
+            try
+            {
+                foreach (var node in nodes)
+                {
+                    var nodes2 = node.SelectNodes("./div[@class='d--fl fw--w']/div[2]/a");
+                    foreach (var node2 in nodes2)
+                    {
+                        colors.Add(node2.SelectSingleNode(".").GetAttributeValue("data-et-prop-listing_color", ""));
+                    }
+                }
+            }
+            catch (NullReferenceException e)
+            {
+                Console.WriteLine("Error referring to Description: " + e);
+            }
+            return colors;
+        }
+
+        public HtmlNodeCollection GetProductPage()
+        { 
+            // Web page with product pages
+            var htmlProduct = "https://poshmark.com" + Item.ProductPageLink;
+            HtmlWeb webProduct = new HtmlWeb();
+            var htmlDocProduct = webProduct.Load(htmlProduct);
+            var nodesProduct = htmlDocProduct.DocumentNode.SelectNodes("//div[@class='listing__layout-grid listing__layout-item listing__info col-x24 col-m12']");
+
+            return nodesProduct;
+        }
+
+        public List<PoshmarkItem> List() 
+        {
+            List<PoshmarkItem> items = new List<PoshmarkItem>();
+
+            // Web page with all the listings of an user
+            var htmlCloset = @"https://poshmark.com/closet/napelvs";
+            HtmlWeb webCloset = new HtmlWeb();
+            var htmlDocCloset = webCloset.Load(htmlCloset);
+            var nodesCloset = htmlDocCloset.DocumentNode.SelectNodes("//div[@id='tiles-con']/div[@class='col-x12 col-l6 col-s8']");
+;
+
+            foreach (var nodeCloset in nodesCloset)
+            {
+                Item = new PoshmarkItem(); // Create new PoshmarkItem object to be populated
+
+                Item.Title = GetTitle(nodeCloset);
+                Item.Price = GetPrice(nodeCloset);
+                Item.NotForSale = GetNotForSale(nodeCloset);
+                Item.Size = GetSize(nodeCloset);
+                Item.Brand = GetBrand(nodeCloset);
+                Item.ProductPageLink = GetProductPageLink(nodeCloset); // Get link for the product page 
+
+                HtmlNodeCollection nodesProduct = GetProductPage(); 
+
+                Item.Description = GetDescription(nodesProduct);
+                Item.Categories = GetCategories(nodesProduct); // Categories requires all the nodes as it has a foreach loop inside
+                Item.Color = GetColor(nodesProduct); 
+
+                items.Add(Item);
+            }
+            return items;
+        }
+    }
+}
---
+++ tests/PoshmarkTests.cs
@@ -0,0 +1,36 @@
+﻿using lib.poshmark_client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace tests
+{
+    public class PoshmarkTests
+    {
+        [Fact]
+        public void List()
+        {
+            var PoshmarkClient = new PoshmarkClient();
+           var foo = PoshmarkClient.List();
+
+            Assert.Equal("/listing/Gray-Pants-5f01cf1f60fdedb0ffff8717", foo[0].ProductPageLink);
+            Assert.Equal("Women", foo[0].Categories[0]);
+            Assert.Equal("Jeans", foo[0].Categories[1]);
+            Assert.Equal("Straight Leg", foo[0].Categories[2]);
+            Assert.Equal("Gray Pants", foo[0].Title);
+            Assert.Equal(100.0, foo[0].Price);
+            Assert.False(foo[0].NotForSale);
+            Assert.Equal("Size: 0", foo[0].Size);
+            Assert.Equal("Signature by Levi Strauss", foo[0].Brand);
+            Assert.Equal("/listing/Meet-your-Posher-Nora-5e98ceee92ea0f645a1a4114", foo[1].ProductPageLink);
+            Assert.Equal("Women", foo[1].Categories[0]);
+            Assert.Equal("Other", foo[1].Categories[1]);
+            Assert.Equal("Meet your Posher, Nora", foo[1].Title);
+            Assert.Equal(0.0, foo[1].Price);
+            Assert.True(foo[1].NotForSale);
+            Assert.Null(foo[1].Size);
+            Assert.Equal("Meet the Posher", foo[1].Brand);
+        }
+    }
+}
